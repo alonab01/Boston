faed6729022a1b0c51608cfdfbb02ec88eb5c22c
Alon Abudraham <alonabudraham2@gmail.com>
2026-01-30 08:44:15 +0200

    the most basic BOSTON 2

--- BashScripts/rebuildVMs.sh
+++ BashScripts/rebuildVMs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-VM_PATH="/home/fwilke/edu/BU/ec721"
+VM_PATH="~/vms"
+USER="alonab01"
 
 
 #start base VM
-sudo qemu-system-x86_64 \
+qemu-system-x86_64 \
 -enable-kvm \
 -m 2048 \
 -drive file=$VM_PATH/base.qcow2,if=virtio \
@@ -13,8 +14,8 @@ sudo qemu-system-x86_64 \
 sleep 20  # wait for VM to boot up
 
 #pull git repo again and rebuild unionbuster
-ssh -p 2222 root@localhost << EOF
-    cd ~/unionbuster
+ssh -p 2222 $USER@localhost << EOF
+    cd ~/Documents/Boston
     git pull
     make clean
     make
@@ -22,7 +23,7 @@ EOF
 
 
 #shutdown base VM
-ssh -p 2222 root@localhost << EOF
+ssh -p 2222 r$USER@localhost << EOF
     poweroff
 EOF
 sleep 5
@@ -33,8 +34,8 @@ pgrep -fa 'qemu-system-x86_64.*vm.*.qcow2'
 echo ""
 
 #clone base VM to vmA and vmB
-sudo rm -f $VM_PATH/vmA.qcow2
-sudo rm -f $VM_PATH/vmB.qcow2
+# sudo rm -f $VM_PATH/vmA.qcow2
+# sudo rm -f $VM_PATH/vmB.qcow2
 
 sudo qemu-img create -f qcow2 -b $VM_PATH/base.qcow2 -F qcow2 $VM_PATH/vmA.qcow2
 sudo qemu-img create -f qcow2 -b $VM_PATH/base.qcow2 -F qcow2 $VM_PATH/vmB.qcow2
